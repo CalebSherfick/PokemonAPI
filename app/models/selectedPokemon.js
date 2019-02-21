142169495b9df2ed62fb96a15e4928db36bd0832
Caleb Sherfick <calebsherfick@gmail.com>
2019-02-20 19:50:09 -0700

    Added card with name number and img

--- app/models/selectedPokemon.js
+++ app/models/selectedPokemon.js
@@ -1,18 +1,24 @@
 export default class SelectedPokemon {
   constructor(data) {
-    this.base_experience = data.base_experience
+    this.id = data.id
+    this.name = data.name
+    this.img = data.sprites.front_shiny || data.img
   }
-
-
-
-
 
 
   getSelBtn() {
     return `
-        <div class="col-3">
-        <h1>${this.base_experience}</h1>
+        <div class="col-12 d-flex justify-content-center align-items-center">
+        <div class="card">
+        <h4 class="card-title">Pokedex Number: ${this.id}</h4>
+  <img class="card-img-top" src=${this.img}>
+    <div class="card-body">
+      <h4 class="card-title">Name: ${this.name}</h4>
+        </div>
+        </div>
         </div>
         `
   }
 }
+
+
